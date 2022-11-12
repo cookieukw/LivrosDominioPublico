1d2187d7bd9d1d3b3190a4920feafac5d0d5867d
CookieUkw <erdesouzapereira@gmail.com>
2022-11-12 17:02:42 -0300

    Adicionando 100 livros

--- move.js
+++ move.js
@@ -1,8 +1,8 @@
 //A pasta pdf é ignorada para o git, então esse script move alguns arquivos para a pasta que será upada para o github. Isso faz com que eu possa enviar aos  e impedir o lotamento de armazenamento da máquina 
 
 const fs= require("fs")
-const frame = "./livros/"
-const pdf = "./book/"
+const pdf = "./livros/"
+const frame = "./books/"
 const list = fs.readdirSync(pdf)
 let size = 100
 let skip = 0
